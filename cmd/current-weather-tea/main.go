f012963dbf2ae1fd5d2c8606554f6f0ae482292d
c-jaenicke <me@chrisj.de>
2023-03-07 16:04:51 +0100

    Working on tui

---
+++ cmd/current-weather-tea/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"weather-go/pkg/env"
+	"weather-go/pkg/weather"
+)
+
+func main() {
+	var pathEnv string
+	flag.StringVar(&pathEnv, "env", "./.env", "Path to .env file containing api key and optional location")
+	flag.Parse()
+
+	env.Path = pathEnv
+
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+}
+
+// currentWeather gets current weather from a location, returns as formatted string
+func currentWeather(m Model) string {
+	weatherData, locationName := weather.GetWeatherData(m.location)
+	return fmt.Sprintf("%s: \n\t%s, %s\n\tTemperature: %s °C\n\tHumidity: %d hpa\n\tPressure: %d\n\tWind: %f m/s from %d", locationName, weatherData.Current.Weather[0].Main, weatherData.Current.Weather[0].Description, strconv.FormatFloat(weatherData.Current.Temp, 'f', -1, 64), weatherData.Current.Humidity, weatherData.Current.Pressure, weatherData.Current.WindSpeed, weatherData.Current.WindDeg)
+}
+
+func getForecast(m Model) string {
+	forecastObject, _ := weather.GetForecast(m.location)
+
+	var sb strings.Builder
+
+	fmt.Println(len(forecastObject.List))
+
+	for index, i := range forecastObject.List {
+		if index == 5 {
+			break
+		}
+		text := fmt.Sprintf("\n%s"+
+			"\n%s"+
+			"\n\tTemp: %s °C"+
+			"\n\tHumidity: %d%%"+
+			"\n\tPressure: %d hpa"+
+			"\n\tWind: %s m/s from %d"+
+			"\n\n", unixToDateTime(i.Dt), i.Weather[0].Description, shortenFloat(i.Main.Temp), i.Main.Humidity, i.Main.Pressure, shortenFloat(i.Wind.Speed), i.Wind.Deg)
+
+		sb.WriteString(text)
+	}
+
+	return sb.String()
+}
+
+// unixToDateTime transforms unix time as int to a datetime string, formatted as "YYYY-MM-DD HH-MM UTC-OFFSET TIMEZONE"
+func unixToDateTime(unix int) string {
+	tm := time.Unix(int64(unix), 0)
+	return tm.String()
+}
+
+// shortenFloat transforms the given float64 value into a string with 2 decimal values
+func shortenFloat(value float64) string {
+	return strconv.FormatFloat(value, 'f', -1, 64)
+}
---
+++ cmd/tabs-tea/ui.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"strings"
+)
+
+type Model struct {
+	textInput  textinput.Model // location input
+	Tabs       []string
+	TabContent []string
+	activeTab  int
+	forecast   string
+	location   string
+	weather    string
+	err        string
+}
+
+func (m Model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func initialModel() Model {
+	input := textinput.New()
+	input.Placeholder = "Location"
+	input.Focus()
+	input.Width = 100
+
+	return Model{
+		textInput: input,
+		forecast:  "",
+		location:  "",
+		weather:   "",
+		err:       "",
+	}
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			// If input is empty, do nothing
+			if m.textInput.Value() == "" {
+				m.err = "Error: No location given!"
+				return m, cmd
+			} else {
+				m.location = m.textInput.Value()
+				m.weather = currentWeather(m)
+				// m.forecast = getForecast(m)
+			}
+		case "right", "l", "n", "tab":
+			m.activeTab = min(m.activeTab+1, len(m.Tabs)-1)
+			return m, nil
+		case "left", "h", "p", "shift+tab":
+			m.activeTab = max(m.activeTab-1, 0)
+			return m, nil
+		}
+
+	}
+
+	// Refresh input field on input
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	doc := strings.Builder{}
+
+	var renderedTabs []string
+
+	for i, t := range m.Tabs {
+		var style lipgloss.Style
+		isFirst, isLast, isActive := i == 0, i == len(m.Tabs)-1, i == m.activeTab
+		if isActive {
+			style = activeTabStyle.Copy()
+		} else {
+			style = inactiveTabStyle.Copy()
+		}
+		border, _, _, _, _ := style.GetBorder()
+		if isFirst && isActive {
+			border.BottomLeft = "│"
+		} else if isFirst && !isActive {
+			border.BottomLeft = "├"
+		} else if isLast && isActive {
+			border.BottomRight = "│"
+		} else if isLast && !isActive {
+			border.BottomRight = "┤"
+		}
+		style = style.Border(border)
+		renderedTabs = append(renderedTabs, style.Render(t))
+	}
+
+	row := lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...)
+	doc.WriteString(row)
+	doc.WriteString("\n")
+	doc.WriteString(windowStyle.Width((lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize())).Render(m.TabContent[m.activeTab]))
+	return docStyle.Render(doc.String())
+}
+
+func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
+	border := lipgloss.RoundedBorder()
+	border.BottomLeft = left
+	border.Bottom = middle
+	border.BottomRight = right
+	return border
+}
+
+var (
+	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
+	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
+	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
+	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
+	activeTabStyle    = inactiveTabStyle.Copy().Border(activeTabBorder, true)
+	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
+)
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
